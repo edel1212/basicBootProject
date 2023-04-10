d8110e84f09829c10f6f85827c440ae7be5a7909
yoo <edel1212@naver.com>
2023-04-10 15:58:50 +0900

    BaseEntity 추가 및 Audutubg 설정

--- src/main/java/com/yoo/basicBoot/BasicBootApplication.java
+++ src/main/java/com/yoo/basicBoot/BasicBootApplication.java
@@ -2,8 +2,10 @@ package com.yoo.basicBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BasicBootApplication {
 
 	public static void main(String[] args) {
