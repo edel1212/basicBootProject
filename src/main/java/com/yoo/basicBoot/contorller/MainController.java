f0b4cebd82e85ac08afdf0289c66f47f7795c1de
yoo <edel1212@naver.com>
2023-04-10 11:07:39 +0900

    add BootStrap

---
+++ src/main/java/com/yoo/basicBoot/contorller/MainController.java
@@ -0,0 +1,17 @@
+package com.yoo.basicBoot.contorller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(){
+        log.info("main Page!!");
+        return "/index";
+    }
+
+}
