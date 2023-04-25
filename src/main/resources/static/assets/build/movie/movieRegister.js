40c1d98474b47887d3debf896ff6992f9c5e224b
yoo <edel1212@naver.com>
2023-04-25 22:25:32 +0900

    등록 UI 작업 끝

--- src/main/resources/static/assets/build/movie/movieRegister.js
+++ src/main/resources/static/assets/build/movie/movieRegister.js
@@ -10,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 class RegisterMovieImpl {
     constructor() {
-        var _a;
+        var _a, _b;
         // 스크롤 옵션
         this.options = {
             root: document.querySelector("#serchBox"),
@@ -28,15 +28,11 @@ class RegisterMovieImpl {
                     .then(res => res.json())
                     .then(result => {
                     // 초기화
+                    this.refreshSearchBox();
                     const searchBox = document.querySelector("#serchBox");
                     if (searchBox instanceof HTMLElement) {
-                        searchBox.style.height = "0px";
                         searchBox.style.height = "300px";
                     } //if
-                    const searchBoxTbody = document.querySelector("#serchBox tbody");
-                    if (searchBoxTbody instanceof HTMLElement) {
-                        searchBoxTbody.innerHTML = "";
-                    }
                     // 결과 Draw
                     this.drawMoiveList(result.results);
                     // 조회결과 업데이트
@@ -47,6 +43,7 @@ class RegisterMovieImpl {
                         total_pages: result.total_pages,
                     };
                 }).then(addEvent => {
+                    // 스크롤 이벤트 추가
                     const lastTr = document.querySelector("#movieLst tr:last-child");
                     if (!lastTr)
                         return;
@@ -56,6 +53,7 @@ class RegisterMovieImpl {
                 });
             } //if        
         };
+        // 영화 목록 스크롤 조회
         this.observer = new IntersectionObserver((entries, observer) => {
             entries.forEach((entry) => __awaiter(this, void 0, void 0, function* () {
                 var _a;
@@ -74,18 +72,92 @@ class RegisterMovieImpl {
                 }
             }));
         }, this.options);
+        // 조회 목록 클릭 이벤트
+        this.getMovieDetailInfo = (event) => {
+            var _a;
+            const clickedElement = event.target;
+            if (!(clickedElement instanceof HTMLElement))
+                return;
+            const clickedElementName = clickedElement.nodeName;
+            if (clickedElementName !== "TD")
+                return;
+            const targetElem = clickedElement.parentElement;
+            const obj = (_a = this.movieSearchResult) === null || _a === void 0 ? void 0 : _a.results.find(i => {
+                return i.id == (targetElem === null || targetElem === void 0 ? void 0 : targetElem.dataset.id);
+            });
+            // 장르 변환
+            let genrePare = [];
+            if (obj === null || obj === void 0 ? void 0 : obj.genre_ids) {
+                for (let i = 0; i < obj.genre_ids.length; i++) {
+                    obj.genre_ids[i];
+                    for (let j = 0; j < this.genreLst.genres.length; j++) {
+                        if (this.genreLst.genres[j].id !== obj.genre_ids[i])
+                            continue;
+                        genrePare.push(this.genreLst.genres[j].name);
+                    } //for
+                } //for
+            }
+            // 데이터 세팅
+            this.movieDetailInfo = {
+                original_title: obj === null || obj === void 0 ? void 0 : obj.original_title,
+                popularity: obj === null || obj === void 0 ? void 0 : obj.popularity,
+                overview: obj === null || obj === void 0 ? void 0 : obj.overview,
+                poster_path: obj === null || obj === void 0 ? void 0 : obj.poster_path,
+                backdrop_path: obj === null || obj === void 0 ? void 0 : obj.backdrop_path,
+                title: obj === null || obj === void 0 ? void 0 : obj.title,
+                release_date: obj === null || obj === void 0 ? void 0 : obj.release_date,
+                id: obj === null || obj === void 0 ? void 0 : obj.id,
+                genre: genrePare,
+            };
+            // UI 세팅
+            this.drawMoiveDetail(this.movieDetailInfo);
+            // 초기화
+            this.refreshSearchBox();
+            // 상세정보 UI
+            const oberviewWrap = document.querySelector("#oberviewWrap");
+            if (oberviewWrap instanceof HTMLElement) {
+                oberviewWrap.style.display = "block";
+            }
+        };
+        // 검색창 UI 및 데이터 초기화
+        this.refreshSearchBox = () => {
+            // 데이터 초기화
+            this.movieSearchResult = {
+                keyword: '',
+                page: 0,
+                results: [],
+                total_pages: 0, // 총 페이지수
+            };
+            // 초기화
+            const searchBox = document.querySelector("#serchBox");
+            if (searchBox instanceof HTMLElement) {
+                searchBox.style.height = "0px";
+            } //if
+            const searchBoxTbody = document.querySelector("#serchBox tbody");
+            if (searchBoxTbody instanceof HTMLElement) {
+                searchBoxTbody.innerHTML = "";
+            }
+            // 상세정보 UI
+            const oberviewWrap = document.querySelector("#oberviewWrap");
+            if (oberviewWrap instanceof HTMLElement) {
+                oberviewWrap.style.display = "none";
+            }
+        };
         // 영화 검색
         this.findMovieBtn = document.querySelector("#findMovieBtn");
         (_a = this.findMovieBtn) === null || _a === void 0 ? void 0 : _a.addEventListener("click", this.getMovieInfo);
-        // 영화 검색 결과 스크롤 이벤트
-        this.findMovieLstElem = document.querySelector("#serchBox");
+        // 조회된 결과 Click
+        this.searchResultElem = document.querySelector("#movieLst");
+        (_b = this.searchResultElem) === null || _b === void 0 ? void 0 : _b.addEventListener("click", this.getMovieDetailInfo);
+        //장르 값 설정
+        this.getGenre();
     } // constructor
     // 영화 조회결과 Draw
     drawMoiveList(objArr) {
         var _a;
         let liHtml = "";
         for (const item of objArr) {
-            liHtml += `<tr>`;
+            liHtml += `<tr data-id="${item.id}">`;
             liHtml += `<td class="backTd" style="background-image: url( 'https://image.tmdb.org/t/p/w200/${item.backdrop_path}' )"></td>`;
             liHtml += `<td class="movieTitle">${item.title}</td>`;
             liHtml += `<td>${item.release_date}</td>`;
@@ -93,6 +165,7 @@ class RegisterMovieImpl {
         } //for   
         (_a = document.querySelector("#movieLst tbody")) === null || _a === void 0 ? void 0 : _a.insertAdjacentHTML("beforeend", liHtml);
     }
+    // Api Call - 영화 목록  
     callApi(page, keyword, tmpArr) {
         return __awaiter(this, void 0, void 0, function* () {
             fetch(`https://api.themoviedb.org/3/search/movie?api_key=a3af7d97effb973e78c5fb1fd7787b13&language=ko-KR&page=${page + 1}&query=${keyword}`)
@@ -117,6 +190,50 @@ class RegisterMovieImpl {
                 console.log(error);
             });
         });
+    }
+    // Api Call - 영화 장르 종류 Set
+    getGenre() {
+        fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=a3af7d97effb973e78c5fb1fd7787b13&language=ko-KR`)
+            .then(res => res.json())
+            .then(result => {
+            this.genreLst = result;
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+    // 선택된 상세정보를 작성
+    drawMoiveDetail(movieDetailInfo) {
+        var _a;
+        // 장르
+        const genres = document.querySelector("#genres");
+        // 인기점수
+        const popularity = document.querySelector("#popularity");
+        // 개봉일        
+        const releaseDate = document.querySelector("#releaseDate");
+        // 요약
+        const overview = document.querySelector("#overview");
+        // 오리지널 타이틀
+        const originalTitle = document.querySelector("#originalTitle");
+        // 포스터 이미지
+        const posterImg = document.querySelector("#posterImg");
+        if (genres instanceof HTMLInputElement) {
+            genres.value = ((_a = movieDetailInfo.genre) === null || _a === void 0 ? void 0 : _a.join(",")) || "장르를 찾을 수 없습니다.";
+        }
+        if (popularity instanceof HTMLInputElement) {
+            popularity.value = movieDetailInfo.popularity || "0.0";
+        }
+        if (releaseDate instanceof HTMLInputElement) {
+            releaseDate.value = movieDetailInfo.release_date || "개봉일자를 찾을 수 없습니다.";
+        }
+        if (overview instanceof HTMLTextAreaElement) {
+            overview.value = movieDetailInfo.overview || "";
+        }
+        if (originalTitle instanceof HTMLInputElement) {
+            originalTitle.value = movieDetailInfo.original_title || "";
+        }
+        if (posterImg instanceof HTMLImageElement) {
+            posterImg.src = `https://image.tmdb.org/t/p/w500/${movieDetailInfo.poster_path}`;
+        }
     }
 }
 // init
--- src/main/resources/static/assets/ts/movie/movieRegister.ts
+++ src/main/resources/static/assets/ts/movie/movieRegister.ts
@@ -2,21 +2,29 @@
  * 영화 글 작성
  */
 interface RegisterMovie{
+    // 영화 정보를 가져옴
     getMovieInfo(event:MouseEvent):void;
+    getMovieDetailInfo(event:MouseEvent):void;
 }
 
 type MovieDetailInfo = {
-    backdrop_path : string;
-    title         : string;
-    release_date  : string;   
-    id?           : number
+    popularity?    : string;    // 인기점수
+    original_title?: string;    // 오리지널 타이틀
+    overview?      : string;    // 요약
+    poster_path?   : string;    // 포스터
+    backdrop_path? : string;    // 배경이미지
+    title?         : string;    // 제목
+    release_date?  : string;    // 개봉일
+    id?            : number;    // ID
+    genre_ids?     : number[];  // 장르ID
+    genre?         : string[];  // 장르변환값
 }
 
 type MovieSearchResult = {
-    keyword?        : string;           // 검색어
-    page            : number;           // 현재페이지
-    results         : MovieDetailInfo[];  // 목록
-    total_pages?    : number;           // 총 페이지수
+    keyword?        : string;               // 검색어
+    page            : number;               // 현재페이지
+    results         : MovieDetailInfo[];    // 목록
+    total_pages?    : number;               // 총 페이지수
 }
 
 class RegisterMovieImpl implements RegisterMovie{
@@ -31,20 +39,30 @@ class RegisterMovieImpl implements RegisterMovie{
     // 영화 검색버튼
     private readonly findMovieBtn;
     
-    // 영화 조회 결과창
-    private findMovieLstElem;
+    // 조회결과
+    private readonly searchResultElem;
 
     // 조회 결과
     private movieSearchResult:MovieSearchResult | undefined;
+
+    // 조회 결과 상세
+    private movieDetailInfo:MovieDetailInfo | undefined;
+    
+    // 장르목록
+    private genreLst :any | undefined;
 
     constructor(){
         // 영화 검색
         this.findMovieBtn = document.querySelector("#findMovieBtn");
         this.findMovieBtn?.addEventListener("click",this.getMovieInfo);
-        
-        // 영화 검색 결과 스크롤 이벤트
-        this.findMovieLstElem = document.querySelector("#serchBox");
-       
+               
+        // 조회된 결과 Click
+        this.searchResultElem = document.querySelector("#movieLst");        
+        this.searchResultElem?.addEventListener("click",this.getMovieDetailInfo);
+
+        //장르 값 설정
+        this.getGenre();        
+
     }// constructor
 
     // 영화 정보를 가져옴
@@ -58,16 +76,12 @@ class RegisterMovieImpl implements RegisterMovie{
             .then(result => {                        
 
                 // 초기화
+                this.refreshSearchBox();
                 const searchBox = document.querySelector("#serchBox");
                 if(searchBox instanceof HTMLElement){
-                    searchBox.style.height = "0px"; 
                     searchBox.style.height = "300px";                       
                 }//if
-                const searchBoxTbody = document.querySelector("#serchBox tbody");
-                if(searchBoxTbody instanceof HTMLElement){
-                    searchBoxTbody.innerHTML = "";
-                }        
-
+                 
                 // 결과 Draw
                 this.drawMoiveList(result.results);
                 
@@ -80,6 +94,7 @@ class RegisterMovieImpl implements RegisterMovie{
                 }                
 
             }).then(addEvent=>{
+                // 스크롤 이벤트 추가
                 const lastTr = document.querySelector("#movieLst tr:last-child");
                 if(!lastTr) return;
                 this.observer.observe(lastTr);               
@@ -93,7 +108,7 @@ class RegisterMovieImpl implements RegisterMovie{
     private drawMoiveList(objArr:MovieDetailInfo[]){
         let liHtml = "";        
         for(const item of objArr){                    
-            liHtml += `<tr>`;
+            liHtml += `<tr data-id="${item.id}">`;
             liHtml += `<td class="backTd" style="background-image: url( 'https://image.tmdb.org/t/p/w200/${item.backdrop_path}' )"></td>`;
             liHtml += `<td class="movieTitle">${item.title}</td>`;
             liHtml += `<td>${item.release_date}</td>`;
@@ -102,7 +117,7 @@ class RegisterMovieImpl implements RegisterMovie{
         document.querySelector("#movieLst tbody")?.insertAdjacentHTML("beforeend",liHtml);
     }
 
-
+    // 영화 목록 스크롤 조회
     private observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(async entry => {
           if (entry.isIntersecting) {
@@ -119,7 +134,8 @@ class RegisterMovieImpl implements RegisterMovie{
           }
         });
       }, this.options);
-  
+    
+    // Api Call - 영화 목록  
     private async callApi(page:number, keyword:string, tmpArr: MovieDetailInfo[]){
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=a3af7d97effb973e78c5fb1fd7787b13&language=ko-KR&page=${page+1}&query=${keyword}`)
             .then(res => res.json())
@@ -144,6 +160,135 @@ class RegisterMovieImpl implements RegisterMovie{
             })
     }
 
+    // Api Call - 영화 장르 종류 Set
+    private getGenre():void{
+        fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=a3af7d97effb973e78c5fb1fd7787b13&language=ko-KR`)
+        .then(res => res.json())
+        .then(result => {                             
+           this.genreLst = result;
+        }).catch(error => {
+            console.log(error);
+        })       
+    }
+    
+
+    // 조회 목록 클릭 이벤트
+    public getMovieDetailInfo = (event : Event)=>{
+        const clickedElement= event.target;
+    
+        if( !(clickedElement instanceof HTMLElement)) return;
+
+        const clickedElementName = clickedElement.nodeName;
+
+        if(clickedElementName !== "TD") return;
+
+        const targetElem = clickedElement.parentElement;
+        
+        const obj = this.movieSearchResult?.results.find(i=>{
+            return i.id == targetElem?.dataset.id
+        })
+
+        // 장르 변환
+        let genrePare:string[] = [];
+        if(obj?.genre_ids){
+            for(let i = 0 ; i < obj.genre_ids.length ; i++){
+                obj.genre_ids[i]
+                for(let j = 0 ; j < this.genreLst.genres.length ; j++){
+                    if(this.genreLst.genres[j].id !== obj.genre_ids[i]) continue;
+                    genrePare.push(this.genreLst.genres[j].name);
+                }//for
+            }//for
+        }    
+        
+        // 데이터 세팅
+        this.movieDetailInfo = {
+            original_title: obj?.original_title,
+            popularity    : obj?.popularity,
+            overview      : obj?.overview,
+            poster_path   : obj?.poster_path,
+            backdrop_path : obj?.backdrop_path,
+            title         : obj?.title,
+            release_date  : obj?.release_date,
+            id            : obj?.id,
+            genre         : genrePare,            
+        }
+
+        // UI 세팅
+        this.drawMoiveDetail(this.movieDetailInfo);
+        
+        // 초기화
+        this.refreshSearchBox();
+
+        // 상세정보 UI
+        const oberviewWrap = document.querySelector("#oberviewWrap");
+        if(oberviewWrap instanceof HTMLElement){
+            oberviewWrap.style.display = "block";
+        }
+
+    }
+
+    // 선택된 상세정보를 작성
+    private drawMoiveDetail(movieDetailInfo : MovieDetailInfo){
+        // 장르
+        const genres  = document.querySelector("#genres");
+        // 인기점수
+        const popularity = document.querySelector("#popularity");
+        // 개봉일        
+        const releaseDate = document.querySelector("#releaseDate");
+        // 요약
+        const overview = document.querySelector("#overview");
+        // 오리지널 타이틀
+        const originalTitle = document.querySelector("#originalTitle");
+        // 포스터 이미지
+        const posterImg = document.querySelector("#posterImg");
+
+        if(genres instanceof HTMLInputElement){
+            genres.value = movieDetailInfo.genre?.join(",") || "장르를 찾을 수 없습니다.";
+        }
+        if(popularity instanceof HTMLInputElement){
+            popularity.value = movieDetailInfo.popularity || "0.0";
+        }
+        if(releaseDate instanceof HTMLInputElement){
+            releaseDate.value = movieDetailInfo.release_date || "개봉일자를 찾을 수 없습니다."; 
+        }
+        if(overview instanceof HTMLTextAreaElement){
+            overview.value = movieDetailInfo.overview || ""; 
+        }
+        if(originalTitle instanceof HTMLInputElement){
+            originalTitle.value = movieDetailInfo.original_title || ""; 
+        }
+        if(posterImg instanceof HTMLImageElement){
+            posterImg.src = `https://image.tmdb.org/t/p/w500/${movieDetailInfo.poster_path}`;             
+        }
+        
+    }
+
+    // 검색창 UI 및 데이터 초기화
+    private refreshSearchBox = ()=>{
+
+        // 데이터 초기화
+        this.movieSearchResult = {
+            keyword        : '' ,       // 검색어
+            page           : 0  ,       // 현재페이지
+            results        : [] ,       // 목록
+            total_pages    : 0  ,       // 총 페이지수
+        }
+
+        // 초기화
+        const searchBox = document.querySelector("#serchBox");
+        if(searchBox instanceof HTMLElement){
+            searchBox.style.height = "0px";                               
+        }//if
+        const searchBoxTbody = document.querySelector("#serchBox tbody");
+        if(searchBoxTbody instanceof HTMLElement){
+            searchBoxTbody.innerHTML = "";
+        }
+        // 상세정보 UI
+        const oberviewWrap = document.querySelector("#oberviewWrap");
+        if(oberviewWrap instanceof HTMLElement){
+            oberviewWrap.style.display = "none";
+        }      
+    }
 
 }
 
