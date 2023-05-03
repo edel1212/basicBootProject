85961a873a6cc3a9c87fab6b4abaa943528971bd
yoo <edel1212@naver.com>
2023-05-03 10:15:19 +0900

    Login Page

---
+++ src/main/java/com/yoo/basicBoot/contorller/user/UserLoginController.java
@@ -0,0 +1,13 @@
+package com.yoo.basicBoot.contorller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserLoginController {
+
+    @GetMapping("/login")
+    public void loginPage(){}
+}
