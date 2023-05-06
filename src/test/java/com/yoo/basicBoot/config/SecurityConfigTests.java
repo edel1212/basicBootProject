b4f5329aa48dd0e0a9edc73f2d3073a9a6cb32f1
yoo <edel1212@naver.com>
2023-05-06 14:12:47 +0900

    패스워드 매칭 테스트

--- src/test/java/com/yoo/basicBoot/config/SecurityConfigTests.java
+++ src/test/java/com/yoo/basicBoot/config/SecurityConfigTests.java
@@ -1,6 +1,8 @@
 package com.yoo.basicBoot.config;
 
 
+import com.yoo.basicBoot.entity.user.Member;
+import com.yoo.basicBoot.repository.user.MemberRepository;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ public class SecurityConfigTests {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Description("PasswordEncoder Bean 주입 확인")
     @Test
@@ -24,6 +28,21 @@ public class SecurityConfigTests {
         String encode = passwordEncoder.encode(pw);
         log.info("encode ::: {}", encode);
         log.info("match Result ::: {}" ,passwordEncoder.matches(pw, encode));
+
+    }
+
+
+    @Description("매칭확인")
+    @Test
+    public void passwordMatchTests(){
+        String pw = "111";
+
+        Member member = memberRepository.findByEmail("edel1@naver.com",false);
+        String pwDB = member.getPassword();
+
+        log.info("pwDB ::: {}", pwDB);
+
+        log.info("match Result ::: {}" ,passwordEncoder.matches(pw, pwDB));
 
     }
 
